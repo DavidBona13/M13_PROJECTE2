85ab0f91107355bf810114f9ccebd23ac10eb997
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-04-15 16:42:10 +0200

    dilluns springBoot

---
+++ projecte-mongo-back/projecte-mongo-back/src/main/java/com/sintedia/projectemongoback/ProjecteMongoBackApplication.java
@@ -0,0 +1,13 @@
+package com.sintedia.projectemongoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteMongoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecteMongoBackApplication.class, args);
+	}
+
+}
---
+++ projecte-mongo-back/projecte-mongo-back/src/test/java/com/sintedia/projectemongoback/ProjecteMongoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sintedia.projectemongoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteMongoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
