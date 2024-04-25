fe2b91d7186e25d7ad27b0460db23e50910658ff
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-04-25 15:55:08 +0200

    dijous angular try solution components inside components

--- SINTEDIA_ANGULAR/src/app/header/header.component.ts
+++ SINTEDIA_ANGULAR/src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RegistroComponent } from '../registro/registro.component';
+import { IniciarSesionComponent } from '../iniciar-sesion/iniciar-sesion.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RegistroComponent, IniciarSesionComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
