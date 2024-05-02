745a284c476b1f6e064492a82a5792ca717dd177
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-05-02 15:06:37 +0200

    Spring Boot == projecte

--- SINTEDIA_ANGULAR/src/app/footer/footer.component.ts
+++ SINTEDIA_ANGULAR/src/app/footer/footer.component.ts
@@ -3,11 +3,12 @@ import { AcercaDeSintediaComponent } from '../acerca-de-sintedia/acerca-de-sinte
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { CodigoDeConductaComponent } from '../codigo-de-conducta/codigo-de-conducta.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [AcercaDeSintediaComponent, RouterLink],
+  imports: [AcercaDeSintediaComponent, RouterLink, CodigoDeConductaComponent, AcercaDeSintediaComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
